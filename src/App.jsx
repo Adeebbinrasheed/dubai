e7e047963add12c0df781219c2dbc0fc7508c4ad
Mohammed Munshif <munshif.upcode@gmail.com>
2024-11-27 16:26:20 +0530

    added middle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Components/Banner'
 import Header from './Components/Header'
+import Middle from './Components/middle'
 
 const App = () => {
   return (
    <div className='bg-[#015845] min-h-screen px-14'>
     <Header/>
     <Banner/>
+    <Middle/>
    </div>
   )
 }
